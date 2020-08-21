4a621ec352c0532e85109b3c3f20371eb0c7f105
sihamEgb <siham.egb@gmail.com>
2020-08-21 11:24:53 +0200

    ex 2.7

---
+++ ex2.7.js
@@ -0,0 +1,12 @@
+function basicOp(op,num1,num2){
+	switch (op){
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+		case '*':
+			return num1 * num2;
+		case '/':
+			return num1/num2;
+	}
+}
